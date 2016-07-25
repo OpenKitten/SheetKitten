92a3e88bf4a15c39add5c7b1c0c5b380e60a8af0
Robbert Brandsma <i@robbertbrandsma.nl>
2016-07-25 22:18:05 +0200

    Now that was hard

--- Sources/SheetKitten.swift
+++ Sources/SheetKitten.swift
@@ -1,4 +1,33 @@
-struct SheetKitten {
+import Foundation
 
-    var text = "Hello, World!"
+public protocol SpreadsheetConvertible {
+    var numberOfRowsForSpreadsheet: UInt { get }
+    var spreadsheetColumnTitles: [String] { get }
+    
+    func getDataForSpreadsheet(atRow row: UInt) -> [String]
+}
+
+public func makeCSV(from source: SpreadsheetConvertible) -> String {
+    let numRows = source.numberOfRowsForSpreadsheet
+    let titles = source.spreadsheetColumnTitles
+    
+    func escape(_ string: String) -> String {
+        guard string.characters.contains("\"") || string.characters.contains(",") else {
+            return string
+        }
+        
+        let string = string.replacingOccurrences(of: "\"", with: "\"\"")
+        
+        return "\"\(string)\""
+    }
+    
+    var csv = titles.map(escape).joined(separator: ",")
+    
+    for i in 0..<numRows {
+        let data = source.getDataForSpreadsheet(atRow: i)
+        let rowString = data.map(escape).joined(separator: ",")
+        csv.append("\n\(rowString)")
+    }
+    
+    return csv
 }
--- Tests/SheetKitten/SheetKittenTests.swift
+++ Tests/SheetKitten/SheetKittenTests.swift
@@ -3,9 +3,30 @@ import XCTest
 
 class SheetKittenTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(SheetKitten().text, "Hello, World!")
+        
+        struct TestSheetType : SpreadsheetConvertible {
+            let numberOfRowsForSpreadsheet: UInt = 3
+            var spreadsheetColumnTitles = ["Name", "Nickname", "Function", "City"]
+            
+            func getDataForSpreadsheet(atRow row: UInt) -> [String] {
+                switch row {
+                case 0:
+                    return ["Robbert Brandsma", "Obbut", "Nerd", "Eindhoven"]
+                case 1:
+                    return ["Joannis Orlandos", "Joannis", "Also Nerd", "Eindhoven"]
+                case 2:
+                    return ["Fred \"Wilhelmus\" met de lastige naam", "Harriebob", "Henk, fred, piet, sap en \"sauselina\"", "Overal en nergens"]
+                default:
+                    fatalError()
+                }
+            }
+        }
+        
+        let s = TestSheetType()
+        let csv = makeCSV(from: s)
+        
+        print(csv)
+        
     }
 
 
